6d413efa83e6d1ef35f3de34aff633f322724723
QuynhAnh-31 <quynhanhanh31122005@gmail.com>
2025-06-05 02:28:36 +0000

    qa

--- HomeStay/database/factories/UserFactory.php
+++ HomeStay/database/factories/UserFactory.php
@@ -21,7 +21,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'role' => 'user', // nếu có cột role
+            'role' => 'user',
         ];
     }
 
